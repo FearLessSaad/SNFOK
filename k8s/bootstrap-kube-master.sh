33efd736155b6134884679b7d510a92f6f846353
Muhammad Saad <saadp791@gmail.com>
2025-05-24 22:49:25 +0500

    v0.0.11

---
+++ k8s/bootstrap-kube-master.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "[TASK 1] Pull required containers"
+kubeadm config images pull
+
+echo "[TASK 2] Initialize Kubernetes Cluster"
+kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=10.10.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+echo "[TASK 3] Install HELM"
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
