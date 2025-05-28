9d183bc38bc1fc85022a520f920f5f0b34e279eb
Muhammad Saad <saadp791@gmail.com>
2025-05-29 02:03:36 +0500

    v0.0.31

--- shared/agent_dto/namespace-labels.go
+++ shared/agent_dto/namespace-labels.go
@@ -3,5 +3,5 @@ package agent_dto
 // NamespaceLabels holds a namespace and its associated app labels
 type NamespaceLabels struct {
 	Namespace string
-	Labels    []string
+	Labels    []string `json:"Labels,omitempty"`
 }
